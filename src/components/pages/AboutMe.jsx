3fdc1566bb35d5ff96d0cdfa1374c97d02f055df
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-07 00:55:40 -0500

    Finishing aboutme and added portfolio section

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -6,9 +6,8 @@ export default function AboutMe() {
         Hello! My name is Luc Tourangeau, I am an aspiring software developer
         with a background in pure science. I graduated from Penn State
         University with a B.S. in Physics. After school, I immediately started
-        working in a
-        <a href="https://taylorlab.web.unc.edu/research/">
-          quantitative biology lab at UNC
+        working in a <a href="https://taylorlab.web.unc.edu/research/">
+        quantitative biology lab at UNC
         </a>, Chapel Hill, as a research assistant / programmer. My main
         responsibility in lab was to implement bio-inspired navigational
         algorithms in robots. 
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -4,23 +4,22 @@ import { useState } from "react";
 const imgWidth = 100;
 const imgHeight = 100;
 
-function projectDetails(name, url, img) {
-  return {name, url, img};
-};
+function projectDetails(name, url) {
+  return {name, url};
+}
 
 const projects = [
-  projectDetails("my-react-portfolio", "https://github.com/TurtleNav/my-react-portfolio", )
+  projectDetails("my-react-portfolio", "https://github.com/TurtleNav/my-react-portfolio"),
+  projectDetails("Healthy Living Recipe Creator", "https://github.com/TurtleNav/group2-recipe-app"),
+  projectDetails("My Tech Blog", "https://github.com/TurtleNav/my-tech-blog"),
+  projectDetails("My Weather Dashboard", "https://github.com/TurtleNav/my-weather-dashboard")
 ]
 
 
-function ProjectCard({name, url, img}) {
+function ProjectCard({name, url}) {
   return (
     <div className="project-card" id={name}>
-      <div>
-        <a href={url}>
-          <img src={img} alt={`Stock image of ${name}`} width={imgWidth} height={imgHeight}></img>
-        </a>
-      </div>
+        <a href={url}></a>
     </div>
   );
 }
@@ -29,9 +28,11 @@ export default function Portfolio() {
   return (
     <div>
       <h2>Portfolio</h2>
-      <div id="card-container">
-        <ProjectCard name="my-react-portfolio" url="https://github.com/TurtleNav/my-react-portfolio" img="../../assets/react.svg"/>
-      </div>
+      {/*Map through all projects. React will pester us about using a key so we appease it with a not-so-great key: the index of the project in the original array*/}
+      {projects.map(({name, url}, index) => (
+        <ProjectCard key={index} name={name} url={url}></ProjectCard>
+      ))
+      }
     </div>
   );
 }
