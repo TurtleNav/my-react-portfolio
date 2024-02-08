950e16e163c3668355c2eb4c4b971886ae58a9f8
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-08 02:37:07 -0500

    Adding images to project cards in react is a dud. Need CSS background image

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -17,7 +17,7 @@ const projects = [
 function ProjectCard({name, url}) {
   return (
     <div className="project-card" id={name}>
-        <a href={url}>{name}</a>
+      <a href={url}>{name}</a>
     </div>
   );
 }
